e70b2876d95ffcc3238e72a1f80716e59c4ba3bd
jim-junior <jimjunior854@gmail.com>
2023-10-12 18:17:37 +0300

    fix issues

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,22 +1,45 @@
-import { View, Text, ScrollView } from "react-native"
+import { View, Text, ScrollView, RefreshControl } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
 import HomeHeader from "../components/HomeHeader"
 import Banner from "../components/home/Banner"
 import PrayerRequests from "../components/home/PrayerRequests"
 import PreviousMeeting from "../components/home/PreviousMeetings"
 import News from "../components/home/News"
+import { useCallback, useState } from "react"
+import { useAppData } from "../hooks/app-data"
 
 
 const Home = () => {
+    const [refreshing, setRefreshing] = useState(false);
+    const { getNews, getPrayers, getTestimonies, getMeetings, getUsers, getChatUsers, getNotes } = useAppData()
+    const onRefresh = useCallback(() => {
+        async function refresh() {
+            setRefreshing(true);
+            await getNews()
+            await getPrayers()
+            await getTestimonies()
+            await getMeetings()
+            await getUsers()
+            await getChatUsers()
+            await getNotes()
+            setRefreshing(false);
+        }
+        refresh()
+    }, []);
+
     return (
         <SafeAreaView style={{ backgroundColor: '#fff' }}>
-            <HomeHeader />
-            <ScrollView>
-                <Banner />
-               <PrayerRequests/>
-                <News/>
-               <PreviousMeeting/>
-      
+            <ScrollView style={{ backgroundColor: '#fff' }} refreshControl={
+                <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+            } >
+                <HomeHeader />
+                <ScrollView>
+                    <Banner />
+                    <PrayerRequests />
+                    <News />
+                    <PreviousMeeting />
+
+                </ScrollView>
             </ScrollView>
         </SafeAreaView>
     )
