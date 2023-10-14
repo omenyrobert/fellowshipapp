a8c25848ec5de43160a0c519135208f0219fa33e
jim-junior <jimjunior854@gmail.com>
2023-10-14 09:54:15 +0300

    Add ksks

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,3 +1,3 @@
 
-// export const HOST_URL = "https://fellow-ship-app.onrender.com"
-export const HOST_URL = "http://192.168.43.1:3001"
+export const HOST_URL = "https://fellow-ship-app.onrender.com"
+//export const HOST_URL = "http://192.168.43.1:3001"
